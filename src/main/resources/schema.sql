d8b2e3afd5d90a27448877d8ab433d22eaa241dd
dr33k <dr33k@users.noreply.com>
2022-08-18 09:30:02 +0100

    Testing 2

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,16 +3,16 @@ $$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'booking_status') THEN
        CREATE TYPE booking_status AS ENUM('VALID','USED','EXPIRED','CANCELLED');
-    ENDIF;
+    END IF;
     IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'seat_type') THEN
        CREATE TYPE seat_type AS ENUM('FIRST_CLASS','COACH');
-    ENDIF;
+    END IF;
     IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'user_role') THEN
         CREATE TYPE user_role AS ENUM('PASSENGER','OFFICER','ADMIN');
-    ENDIF;
+    END IF;
     IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'voyage_status') THEN
         CREATE TYPE voyage_status AS ENUM('PENDING','IN_TRANSIT','COMPLETED','CANCELLED');
-    ENDIF;
+    END IF;
 
    END
     $$;
