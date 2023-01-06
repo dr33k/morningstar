7ab1dbae73856327db8369c551a51d3c1f3d3f88
dr33k <dr33k@users.noreply.com>
2023-01-06 19:01:49 +0100

    Security

--- src/main/java/com/seven/ije/config/security/SecurityConfigurer.java
+++ src/main/java/com/seven/ije/config/security/SecurityConfigurer.java
@@ -1,6 +1,7 @@
 package com.seven.ije.config.security;
 
 import com.seven.ije.services.UserService;
+import org.apache.catalina.startup.WebappServiceLoader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
