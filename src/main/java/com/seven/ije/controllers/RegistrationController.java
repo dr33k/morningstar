97c29cfe23bca155427ec4b8605cb1b1c89d4dfe
dr33k <dr33k@users.noreply.com>
2023-01-06 18:03:28 +0100

    Refactored Registration Controller

--- src/main/java/com/seven/ije/controllers/RegistrationController.java
+++ src/main/java/com/seven/ije/controllers/RegistrationController.java
@@ -1,10 +1,9 @@
 package com.seven.ije.controllers;
 
-import com.seven.ije.models.exceptions.ResourceAlreadyExistsException;
-import com.seven.ije.models.records.UserRecord;
 import com.seven.ije.models.requests.UserCreateRequest;
+import com.seven.ije.models.responses.Response;
 import com.seven.ije.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,18 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.util.Set;
+
+import static com.seven.ije.util.Responder.ok;
 
 @RestController
 @RequestMapping("/register")
 public class RegistrationController{
-    @Autowired
     UserService userService;
-
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
     @PostMapping
-    ModelAndView createResource(@Valid @RequestBody UserCreateRequest request) throws ResourceAlreadyExistsException{
-        UserRecord userRecord = UserRecord.copy(request);
-        userRecord = (UserRecord) userService.create(userRecord);
-
-        return new ModelAndView("redirect:/dashboard","userRecord",userRecord);
+    public ResponseEntity<Response> createResource(@Valid @RequestBody UserCreateRequest request){
+        return ok(Set.of(userService.create(request)));
     }
 }
