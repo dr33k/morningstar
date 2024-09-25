1698a34f3cb8f87a00b93a80083e8f402cbfdf9a
dr33k <dr33k@users.noreply.github.com>
2024-09-25 21:59:45 +0100

    Finished setting up backend

--- morningstar-backend/src/main/java/com/seven/morningstar/MorningstarApplication.java
+++ morningstar-backend/src/main/java/com/seven/morningstar/MorningstarApplication.java
@@ -1,13 +1,10 @@
 package com.seven.morningstar;
 
-import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Theme("default")
-public class MorningstarApplication implements AppShellConfigurator {
+public class MorningstarApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(MorningstarApplication.class, args);
 	}
